96a70c2ae851942ba7e75b4119860a422925df53
okane <okanekudasai6@gmail.com>
2024-05-16 09:45:11 +0900

    init

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ directory="/usr/share/nginx/html/assets"
+
+ # 디렉터리가 존재하는지 확인
+ if [ -d "$directory" ]; then
+     echo "디렉터리가 존재합니다. 삭제를 진행합니다."
+     rm -r "$directory"
+ else
+     echo "디렉터리가 존재하지 않습니다. 아무 작업도 수행하지 않습니다."
+ fi
+ sudo mv -f /home/ubuntu/deploy_front/favicon.ico /usr/share/nginx/html/
+ sudo mv -f /home/ubuntu/deploy_front/index.html /usr/share/nginx/html/
+ sudo mv -f /home/ubuntu/deploy_front/assets /usr/share/nginx/html/
+
+ # end of the script
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <div>
+    sfesfe
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(`${import.meta.env.VITE_API_SERVER}/test/hello`);
+  }
+}
+
+</script>
+<style scoped>
+
+</style>
